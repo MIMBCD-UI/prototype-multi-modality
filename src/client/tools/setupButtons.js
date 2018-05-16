eaf694be10c8fc0095abc0dbee5323e6f8025513
FMCalisto <FMCalisto@users.noreply.github.com>
2018-05-16 17:25:01 +0100

    [UPDATE] Tool Bar

--- src/client/tools/setupButtons.js
+++ src/client/tools/setupButtons.js
@@ -88,115 +88,12 @@ const setupButtons = (studyViewer) => {
     });
   });
 
-  // Freehand ROI draw
-  $(buttons[5]).on('click touchstart', function() {
-    disableAllTools();
-    forEachViewport(function(element) {
-      currentElement = element;
-      cornerstoneTools.probe.disable(element);
-      cornerstoneTools.freehand.activate(element, 1);
-    });
-  });
-
-  // Drag Probe
-  $(buttons[6]).on('click touchstart', function() {
-    disableAllTools();
-    forEachViewport(function(element) {
-      currentElement = element;
-      cornerstoneTools.probe.activate(element, 1);
-    });
-  });
-
-  // JSON Save Button
-  $(buttons[7]).on('click touchstart', function() {
-    disableAllTools();
-    forEachViewport(function(element) {
-
-      /**
-       * time_stamp => YYYYMMDDHHmmSS
-       *
-       * Y: Year
-       * M: Month
-       * D: Day
-       * H: Hour
-       * m: minute
-       * S: Second
-       *
-       */
-
-      var freeHandToolData = cornerstoneTools.getToolState(element, 'freehand');
-      var probeToolData = cornerstoneTools.getToolState(element, 'probe');
-      var studyRoiData = studyViewer.roiData;
-      var studyCurrentStack = studyRoiData.currentStack;
-      var stack = studyRoiData.stacks[studyCurrentStack];
-      var dataStudyId = studyRoiData.studyId;
-      var dataPatientId = studyRoiData.patientId;
-      var sourceNameToSave = dataStudyId;
-      var fileNameToSave = sourceNameToSave + fileFormat;
-
-      console.log("Study ROI Data: ", studyRoiData);
-
-      if (freeHandToolData === undefined && probeToolData === undefined) {
-        return;
-      }
-
-      console.log("TimeStamp: ", timeStamp);
-
-      // The name of the file is the system studyId of the patient:
-      // timeStamp + studyId
-      // Example:
-      // 20180223203337 + 123456789
-      // Output File:
-      // 20180223203337123456789.json
-
-      console.log("Saved File Name: ", fileNameToSave);
-      if(freeHandToolData !== undefined){
-        stack.freehand = freeHandToolData.data;
-      }
-      if(probeToolData !== undefined){
-        stack.probe = probeToolData.data;
-      }
-
-
-
-      console.log(studyRoiData);
-
-      // save json
-      // download(
-      //   studyRoiData,
-      //   fileNameToSave,
-      //   'application/octet-stream'
-      // );
-
-      var pathFileNameToSave = fileNameToSave;
-      var data = {
-        rawData: studyRoiData,
-        path: pathFileNameToSave
-      };
-
-      $.ajax({
-        url: '/SaveFile',
-        data: JSON.stringify(data),
-        dataType: "text",
-        type: 'POST',
-        success: function(data) {
-          console.log('File saved successfully on server');
-        },
-        error: function(xhr, status, error) {
-          console.log('Error Occured while saving file');
-        },
-      });
-    });
-  });
-
   // Tooltips
   $(buttons[0]).tooltip();
   $(buttons[1]).tooltip();
   $(buttons[2]).tooltip();
   $(buttons[3]).tooltip();
   $(buttons[4]).tooltip();
-  $(buttons[5]).tooltip();
-  $(buttons[6]).tooltip();
   $(buttons[7]).tooltip();
 
   const download = (data, name, type) => {
